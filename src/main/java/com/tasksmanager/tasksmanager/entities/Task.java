3a78a385f90434910492d7ff80937439aa8a5add
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2021-04-04 01:03:23 -0300

    Started

---
+++ src/main/java/com/tasksmanager/tasksmanager/entities/Task.java
@@ -0,0 +1,61 @@
+package com.tasksmanager.tasksmanager.entities;
+
+import java.io.Serializable;
+
+public class Task implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String description;
+	
+	public Task() {
+	}
+
+	public Task(Long id, String description) {
+		super();
+		this.id = id;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
