50cb232820a21cff4ddf126cd442e47e543ac2e9
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2021-08-22 14:46:43 -0300

    Middle name changed

--- src/main/java/com/tasksmanager/tasksmanager/TasksmanagerApplication.java
+++ src/main/java/com/tasksmanager/tasksmanager/TasksmanagerApplication.java
@@ -36,31 +36,35 @@ public class TasksmanagerApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Position position1 = new Position(null, "Gerente");
-		Position position2 = new Position(null, "Admin");
+		Position position1 = new Position(null, "Admin");
+		Position position2 = new Position(null, "Gerente");
 		Position position3 = new Position(null, "Operador");
 		
-		TeamMember teamMemberAdmin = new TeamMember(null, "admin", "admin@email.com", "123", position2);
+		TeamMember teamMemberAdmin = new TeamMember(null, "admin", "admin@email.com", "123", position1);
 		teamMemberAdmin.addProfile(Profile.ADMIN);
 		
-		TeamMember teamMember1 = new TeamMember(null, "Lucas Santos", "lucas@email.com", "123", position1);
-		TeamMember teamMember2 = new TeamMember(null, "Maria Aparecida", "maria@email.com", "123", position3);
+		TeamMember teamMember1 = new TeamMember(null, "Lucas Coelho", "lucas@email.com", "123", position2);
+		TeamMember teamMember2 = new TeamMember(null, "Henrique Lima", "henrique@email.com", "123", position3);
+		TeamMember teamMember3 = new TeamMember(null, "João Vitor", "joao@email.com", "123", position3);
 		
 		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
 	    Date date = new Date();
 	    String formatedDate = formatter.format(date);
 		
-	    Task task1 = new Task(null, teamMember1, "Revisar implementação da Sprint anterior", formatedDate);
-		Task task2 = new Task(null, teamMember1, "Implementar nova funcionalidade do sistema", formatedDate);
-		Task task3 = new Task(null, teamMember2, "Realizar a entrevista com usuários do novo sistema", formatedDate);
-		Task task4 = new Task(null, teamMember2, "Desenvolver protótipo de tela do novo sistema", formatedDate);
-		Task task5 = new Task(null, teamMember1, "Corrigir erro ao salvar formulário", formatedDate);
-		Task task6 = new Task(null, teamMember1, "Refatorar código já implementado", formatedDate);
-		Task task7 = new Task(null, teamMember2, "Atualizar licensa do software", formatedDate);
-		Task task8 = new Task(null, teamMember2, "Preparar apresentação PPT para o cliente", formatedDate);
+	    Task task1 = new Task(null, teamMember1, "Conferir relatório", formatedDate);
+		Task task2 = new Task(null, teamMember1, "Realizar entrevista técnica", formatedDate);
+		Task task3 = new Task(null, teamMember2, "Conferir estoque", formatedDate);
+		Task task4 = new Task(null, teamMember2, "Receber mercadorias", formatedDate);
+		Task task5 = new Task(null, teamMember1, "Corrigir erro na planilha", formatedDate);
+		Task task6 = new Task(null, teamMember1, "Reportar resultados a central", formatedDate);
+		Task task7 = new Task(null, teamMember2, "Atualizar planilha", formatedDate);
+		Task task8 = new Task(null, teamMember2, "Acompanhar novo funcionário", formatedDate);
+		Task task9 = new Task(null, teamMember3, "Preparar apresentação PPT para o cliente", formatedDate);
+		Task task10 = new Task(null, teamMember3, "Enviar email para gerencia", formatedDate);
 		
 		teamMember1.setTasks(Arrays.asList(task1, task2, task5, task6));
 		teamMember2.setTasks(Arrays.asList(task3, task4, task7, task8));
+		teamMember3.setTasks(Arrays.asList(task9, task10));
 		
 		positionRepository.save(position1);
 		positionRepository.save(position2);
@@ -69,6 +73,7 @@ public class TasksmanagerApplication implements CommandLineRunner{
 		teamMemberRepository.save(teamMemberAdmin);
 		teamMemberRepository.save(teamMember1);
 		teamMemberRepository.save(teamMember2);
+		teamMemberRepository.save(teamMember3);
 		
 		taskRepository.save(task1);
 		taskRepository.save(task2);
@@ -78,6 +83,8 @@ public class TasksmanagerApplication implements CommandLineRunner{
 		taskRepository.save(task6);
 		taskRepository.save(task7);
 		taskRepository.save(task8);
+		taskRepository.save(task9);
+		taskRepository.save(task10);
 	}
 
 }
